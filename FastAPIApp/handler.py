54450f94e6b52bbc40c8e963b22a2bb8a22e8af0
hidecology <hidecology0302@gmail.com>
2023-11-11 10:57:36 +0900

    upgrade to linebot api v3

--- FastAPIApp/handler.py
+++ FastAPIApp/handler.py
@@ -1,12 +1,31 @@
 import os
-from linebot import LineBotApi, WebhookHandler
-from linebot.models import MessageEvent, TextMessage
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage,
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent,
+)
 
 # LINE Botに関するインスタンス作成
-line_bot_api = LineBotApi(os.getenv("LINEOA_CHANNEL_ACCESS_TOKEN",""))
+configuration = Configuration(access_token=os.getenv("LINEOA_CHANNEL_ACCESS_TOKEN",""))
 handler = WebhookHandler(os.getenv("LINEOA_CHANNEL_SECRET",""))
 
-@handler.add(MessageEvent)
+@handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    res_data = TextMessage(text="メッセージを受け取りました")
-    line_bot_api.reply_message(event.reply_token, res_data)
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        res_data = TextMessage(text="メッセージを受け取りました")
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[res_data]
+            )
+        )
